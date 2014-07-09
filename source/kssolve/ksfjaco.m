1f9d3e5e3219bf0d19efe66248f6fde4042f3531
Xiong Ding <xiong@phys32125.physics.gatech.edu>
2014-07-09 14:21:15 -0400

    mex succeed

--- source/kssolve/ksfjaco.m
+++ source/kssolve/ksfjaco.m
@@ -1,14 +1,19 @@
 function [aa, daa] = ksfjaco(a0, h, nstp, d, np, nqr)
+% integrate KS system with/without calculate Jacobian matrix.
+% exmple usage:
+  %   aa = ksfjaco(a0, 0.25, 1000);
+% [aa, daa] = ksfjaco(a0, 0.25, 1000);
+
 if nargin < 6, nqr = 1; end
 if nargin < 5, np = 1; end
 if nargin < 4, d = 22; end
 
-if nargou == 2,
+if nargout == 2,
     [aa, daa] = MEXkssolve(a0, d, h, nstp, np, nqr, 1); 
-elseif nargin == 1,
+elseif nargout == 1,
     aa = MEXkssolve(a0, d, h, nstp, np, 1, 1);
 else
-    fprinf("the number of output is wrong");
+    fprintf(1, 'the number of output is wrong. \n');
 end
 
 end

072789848d16920e3de1ee61f7dcf8cbf2d674aa
dingxiong <dingxiong203@gmail.com>
2014-10-10 15:36:16 -0400

    M1: jacobian

--- source/ksint/ksintM1.cc
+++ source/ksint/ksintM1.cc
@@ -26,6 +26,24 @@ void KSM1::NL(KSfft &f){
   f.vc3 = (a1hat - 1)*L*f.vc1 + a1hat*G*f.vc3 - f.vc3(1).real()*G*f.vc1;
 }
 
+void KSM1::jNL(KSfft &f){
+  double a1 = f.vc1(1,0).real();
+  ArrayXd y1 = f.vc1.block(1,1,1,N-2).real();
+  ifft(f);
+  ArrayXd tmp = f.vr2.col(0);
+  f.vr2 = tmp.matrix().asDiagonal()*f.vr2.matrix();
+  fft(f);
+  double fv1 = f.vc3.row(1).real();
+  /* (q_k^2-q_k^4)*a - iq_k/2*\sum a_m a_{k-m} */
+  f.vc3.col(0) = L*f.vc1.col(0) + G*f.vc3.col(0);
+  /* (q_k^2-q_k^4)*y - iq_k/2*\sum a_m y_{k-m} */
+  f.vc3.rightCols(N-2) = L*f.vc1.rightCols(N-2) + 2.0*G*f.vr3.rightCols(N-2);
+  
+  f.vc3.rightCols(N-2) = (a1-1)*f.vc3.rightCols(N-2) - G * f.vc1.rightCols(N-2)*fv1(0)
+    +y1*f.vr3.col(0) - 2.0*G*fv1.tail(N-2);
+  f.vc3.col(0) = a1*f.vc3.col(0) - L*f.vc1.col(0) -fv1(0)*G*f.vc1;
+}
+
 KSM1::KSat KSM1::intg(const ArrayXd &a0, size_t nstp, size_t np){
   if( N-2 != a0.rows() ) {printf("dimension error of a0\n"); exit(1);} 
   Fv.vc1 = R2C(a0); 
@@ -93,4 +111,16 @@ KSM1::KSat KSM1::intg2(const ArrayXd &a0, double T, size_t np){
   at.aa = aa.leftCols(ix); at.tt = tt.head(ix);
 
   return at;
+}
+
+KSM1::intgj(const ArrayXd a0,  size_t nstp, size_t np, size_t nqr){
+  if( N-2 != a0.rows() ) {printf("dimension error of a0\n"); exit(1);}
+  ArrayXXd v0(N-2, N-1);
+  v0 << a0, MatrixXd::Identity(N-2, N-2);
+  jFv.vc1 = R2C(v0);
+  ArrayXXd aa(N-2, nstp/np+1); aa.col(0) = a0;
+  ArrayXXd daa((N-3)*(N-3), nstp/nqr);
+  for(size_t i = 1; i < nstp + 1; i++){
+    
+  }
 }

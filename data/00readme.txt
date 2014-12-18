501738bbbc643c6dac61ebac24ac26fbbbbef461
Xiong Ding <xiong@hard.physics.gatech.edu>
2014-12-18 10:19:36 -0500

    ignore

--- data/00readme.txt
+++ data/00readme.txt
@@ -5,3 +5,4 @@ copyh5.sh : script to combine data for T < 100 and  100 < T < 120
 converge: 
 	  contains the information about convergence of PED on 
 	  upos.
+120 : angle distriubtion for T < 120
--- data/ErgodicApproch/disAngscatter.py
+++ data/ErgodicApproch/disAngscatter.py
@@ -8,15 +8,18 @@ ax=fig.add_subplot(111)
 
 ##############################
 # prepare the data
-
+"""
 pp1 = sio.loadmat('./ppo4/disAng.mat'); 
 ang1 = pp1['Sang']; dis1 = pp1['Sdis'];
 pp2 = sio.loadmat('./ppo4_old2/disAng.mat')
 ang2 = pp2['Sang']; dis2 = pp2['Sdis'];
 ang = np.vstack((ang1, ang2)); dis = np.vstack((dis1, dis2));
-
+"""
+pp = sio.loadmat('MdisAng_rpo1.mat'); 
+dis = pp['Mdis']; ang = np.sin(np.arccos(pp['Mang'])) + 1e-8;
+ 
 Num = 4;
-cix = [3, 4, 6, 9]; # column index
+cix = [3, 4, 5, 8]; # column index
 spix = [6, 7, 9, 15]; # subspace index
 colors = ['r', 'b', 'c', 'm'];
 markers =['o', 's', 'v', '*'];
@@ -27,16 +30,16 @@ b = -0.7
 for i in range(Num):
     ax.scatter(dis, ang[:,cix[i]], s=7, c=colors[i], marker=markers[i], 
                edgecolor='none', label='1-'+str(spix[i]))
-ax.plot([10**-2.5,10**(-1)],[10**(b-2.5), 10**(b-1)], c='k', lw=2)
+#ax.plot([10**-2.5,10**(-1)],[10**(b-2.5), 10**(b-1)], c='k', lw=2)
 
 ax.set_yscale('log')    
 ax.set_xscale('log')
+ax.set_ylim([1e-5,1])
+ax.set_xlim([1e-4,1e-1])
 
 ax.legend(loc='upper left')
 #ax.set_title('(a)')
 
-#ax.set_xlim((0,0.1))
-#ax.set_ylim()
 ax.set_ylabel(r'$\sin(\theta)$',size='large')
 ax.set_xlabel(r'$||\Delta x||_2$',size='large')
 

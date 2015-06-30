a2a986944bab1b8bcca8467b437fae8bb5f52f2e
Xiong Ding <xiong@hard.physics.gatech.edu>
2015-06-30 18:17:14 -0400

    write rossler integrator

--- applications/rossler/rossler.cc
+++ applications/rossler/rossler.cc
@@ -87,41 +87,65 @@ public:
     Velocity velocity;
     Jacv jacv;
     double odeAtol, odeRtol;
-    int blockSize;
+
 
     Rossler(double odeAtol = 1e-16, double odeRtol = 1e-12,
-	    double blockSize = 100,
 	    double a = 0.2, double b=0.2, double c=5.7) :
 	a(a), b(b), c(c),
 	velocity(a, b, c),
 	jacv(a, b, c),
-	odeAtol(odeAtol), odeRtol(odeRtol),
-	blockSize(blockSize){}
+	odeAtol(odeAtol), odeRtol(odeRtol)
+    {}
     
     template< typename Vel>
     std::pair<ArrayXXd, ArrayXd>
-    intg(const ArrayXd &x0, const double h, const int nstp, Vel vel){
+    intg0(const ArrayXd &x0, const double h, const int nstp, Vel vel, int blockSize = 100){
 	ArrayXXd xx(x0.size(), blockSize);
 	ArrayXd tt(blockSize);
+	int totalNum = 0;
 	std::vector<double> x(&x0[0], &x0[0] + x0.size());
-	StoreStates storeStates(xx, tt, blockSize);
 	integrate_adaptive(make_controlled< runge_kutta_cash_karp54<std::vector<double>> >( odeAtol , odeRtol ) ,
 			   // integrate(
-			   velocity, x, 0.0, nstp*h, h, storeStates);
-	cout << StoreStates::totalNum << endl;
-	return make_pair(xx.leftCols(storeStates.totalNum), tt.head(storeStates.totalNum));
+			   vel, x, 0.0, nstp*h, h,
+			   [&xx, &tt, &totalNum, &blockSize](const std::vector<double> &x, double t){		      
+			       if(totalNum >= tt.size()){
+				   xx.conservativeResize(NoChange, xx.cols() + blockSize);
+				   tt.conservativeResize(tt.size() + blockSize);
+			       }
+			       xx.col(totalNum) = ArrayXd::Map(&x[0], x.size());
+			       tt(totalNum++) = t;
+			   });
+       
+	return make_pair(xx.leftCols(totalNum), tt.head(totalNum));
+    }
+
+    std::pair<ArrayXXd, ArrayXd>
+    intg(const ArrayXd &x0, const double h, const int nstp, int blockSize = 100){
+	return intg0(x0, h, nstp, velocity, blockSize);
+    }
+
+    std::tuple<ArrayXXd, ArrayXXd, ArrayXd>
+    intgj(const ArrayXd &x0, const ArrayXd &dx0, const double h,
+	  const int nstp, int blockSize = 100){
+	ArrayXd xdx(x0.size() + dx0.size());
+	xdx << x0, dx0;
+	auto tmp = intg0(xdx, h, nstp, jacv, blockSize);
+	return std::make_tuple(tmp.first.topRows(3),
+			       tmp.first.bottomRows(3),
+			       tmp.second);
     }
     
 
 };
-int Rossler::StoreStates::totalNum = 0;
 
 
 int main(){
     Rossler ros;
     Array3d x0;
     x0 << 1, 6.0918, 1.2997;
-    auto tmp = ros.intg(x0, 0.01, 588, ros.velocity);
-    //cout << tmp.first << endl;
+    Array3d dx0;
+    dx0 << 0.01, 0.01, 0.01;
+    auto tmp = ros.intgj(x0, dx0, 0.01, 588);
+    cout << std::get<1>(tmp) << endl;
     return 0;
 }

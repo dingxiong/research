ed129ee0d7e0ac2a8ddd83c7a19aa36720f47df2
Xiong Ding <dingxiong203@gmail.com>
2017-03-04 22:44:59 -0500

    KS L=200

--- applications/Orbit_FV/KSchangeDomainSize.py
+++ applications/Orbit_FV/KSchangeDomainSize.py
@@ -1,7 +1,7 @@
 from py_ks import *
 from personalFunctions import *
 
-case = 10
+case = 20
 
 if case == 10:
     """
@@ -14,6 +14,22 @@ if case == 10:
 
     a0 = rand(N-2)
     aa = ks.intg(a0, h, np.int(100/h), 1000000)
+    a0 = aa[-1]
+    aa = ks.intg(a0, h, np.int(T/h), 5)
+    
+    ksp.config(aa, [0, L, 0, T], size=[6, 6], axisLabelSize=25, tickSize=16)
+
+if case == 20:
+    """
+    use a larger domain size for simulation
+    """
+    N, L = 128*2, 200
+    ks = pyKS(N, L)
+    ksp = KSplot(ks)
+    T, h = 100, 0.001
+
+    a0 = rand(N-2)
+    aa = ks.intg(a0, h, np.int(400/h), 1000000)
     a0 = aa[-1]
     aa = ks.intg(a0, h, np.int(T/h), 5)
     

ba0c42d2a8351379a7e73c0c6131da5d7caa1fcc
Xiong Ding <dingxiong203@gmail.com>
2017-04-11 16:08:53 -0400

    boost python intial trial

--- PythonWrapper/ks/py_ks.cc
+++ PythonWrapper/ks/py_ks.cc
@@ -17,12 +17,6 @@ namespace bn = boost::numpy;
 
 
 class pyKS : public KS {
-  /*
-   * Python interface for ksint.cc
-   * Note:
-   *     1) Input and out put are arrays of C form, meaning each
-   *        row is a vector
-   */
   
 public:
     pyKS(int N, double d) : KS(N, d) {}
@@ -39,6 +33,41 @@ public:
 	return copy2bn(hs);
     }
 
+    /* wrap the integrator */
+    bn::ndarray PYintgC(bn::ndarray a0, double h, double tend, int skip_rate){
+	int m, n;
+	getDims(a0, m, n);
+	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
+
+	return copy2bn(intgC(tmpa, h, tend, skip_rate));
+    }
+
+    /* wrap the integrator with Jacobian */
+    bp::tuple PYintgjC(bn::ndarray a0, double h, double tend, int skip_rate){
+	int m, n;
+	getDims(a0, m, n);
+	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
+	auto tmpav = intgjC(tmpa, h, tend, skip_rate);
+	
+	return bp::make_tuple(copy2bn(tmpav.first), copy2bn(tmpav.second));
+    }
+
+    bn::ndarray PYintg(bn::ndarray a0, double h, double tend, int skip_rate){
+	int m, n;
+	getDims(a0, m, n);
+	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
+	return copy2bn(intg(tmpa, h, tend, skip_rate));
+    }
+
+    bp::tuple PYintgj(bn::ndarray a0, double h, double tend, int skip_rate){
+	int m, n;
+	getDims(a0, m, n);
+	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
+	auto result = intgj(tmpa, h, tend, skip_rate);
+	
+	return bp::make_tuple(copy2bn(result.first), copy2bn(result.second));
+    }
+
     /* wrap the velocity */
     bn::ndarray PYvelocity(bn::ndarray a0){
 	int m, n;
@@ -48,12 +77,12 @@ public:
 	return copy2bn(velocity(tmpa));
     }
     
-    bn::ndarray PYvelg(bn::ndarray a0, double theta){
+    bn::ndarray PYvelReq(bn::ndarray a0, double theta){
 	int m, n;
 	getDims(a0, m, n);
 	Map<VectorXd> tmpa((double*)a0.get_data(), n*m);
 	
-	return copy2bn(velg(tmpa, theta));
+	return copy2bn(velReq(tmpa, theta));
     }
     
     /* stab */
@@ -72,54 +101,14 @@ public:
 	
 	return copy2bn(stabReq(tmpa, theta));
     }
-
-    /* wrap the integrator */
-    bn::ndarray PYintg(bn::ndarray a0, double h, int Nt, int skip_rate){
-	int m, n;
-	getDims(a0, m, n);
-	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
-
-	return copy2bn(intg(tmpa, h, Nt, skip_rate));
-    }
-
-    /* wrap the integrator with Jacobian */
-    bp::tuple PYintgj(bn::ndarray a0, double h, int Nt, int skip_rate){
-	int m, n;
-	getDims(a0, m, n);
-	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
-	auto tmpav = intgj(tmpa, h, Nt, skip_rate);
-	
-	return bp::make_tuple(copy2bn(tmpav.first), copy2bn(tmpav.second));
-    }
-
-    bn::ndarray PYaintg(bn::ndarray a0, double h, double tend, int skip_rate){
-	int m, n;
-	getDims(a0, m, n);
-	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
-	return copy2bn(aintg(tmpa, h, tend, skip_rate));
-    }
-
-    bp::tuple PYaintgj(bn::ndarray a0, double h, double tend, int skip_rate){
-	int m, n;
-	getDims(a0, m, n);
-	Map<ArrayXd> tmpa((double*)a0.get_data(), n*m);
-	auto result = aintgj(tmpa, h, tend, skip_rate);
-	
-	return bp::make_tuple(copy2bn(result.first),
-			      copy2bn(result.second)
-			      );
-    }
     
     /* reflection */
-    bn::ndarray PYreflection(bn::ndarray aa){
-
+    bn::ndarray PYreflect(bn::ndarray aa){
 	int m, n;
-	getDims(aa, m, n);
-	
+	getDims(aa, m, n);	
 	Map<ArrayXXd> tmpaa((double*)aa.get_data(), n, m);
-	return copy2bn(Reflection(tmpaa));
+	return copy2bn(reflect(tmpaa));
     }
-
 
     /* half2whole */
     bn::ndarray PYhalf2whole(bn::ndarray aa){
@@ -129,90 +118,52 @@ public:
 	return copy2bn(half2whole(tmpaa));
     }
 
-
     /* Rotation */
-    bn::ndarray PYrotation(bn::ndarray aa, const double th){
+    bn::ndarray PYrotate(bn::ndarray aa, const double th){
 	int m, n;
 	getDims(aa, m, n);
 	Map<ArrayXXd> tmpaa((double*)aa.get_data(), n, m);
-	return copy2bn(Rotation(tmpaa, th));
+	return copy2bn(rotate(tmpaa, th));
     }
-
-
-    /* orbitToSlice */
-    bp::tuple PYorbitToSlice(bn::ndarray aa){
+        
+    bp::tuple PYredSO2(bn::ndarray aa, int p, bool toY){
 	int m, n;
 	getDims(aa, m, n);
 	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
-	auto result = orbitToSlice(tmpaa);
-	return bp::make_tuple(copy2bn(result.first), copy2bn(result.second));
+	
+	auto tmp = redSO2(tmpaa, p, toY);
+	return bp::make_tuple(copy2bn(tmp.first), copy2bn(tmp.second));
     }
 
-
-    /* veToSlice */
-    bn::ndarray PYveToSlice(bn::ndarray ve, bn::ndarray x){
+    bp::tuple PYfundDomain(bn::ndarray aa, int pSlice, int pFund){
 	int m, n;
-	getDims(ve, m, n);
-	Map<MatrixXd> tmpve((double*)ve.get_data(), n, m);
-	getDims(x, m, n);
-	Map<VectorXd> tmpx((double*)x.get_data(), n*m);
-	return copy2bn(veToSlice(tmpve, tmpx));
-    }
-
-    /* veToSliceAll */
-    bn::ndarray PYveToSliceAll(bn::ndarray eigVecs, bn::ndarray aa, const int trunc){
-	int m, n;
-	getDims(eigVecs, m, n);
-	Map<MatrixXd> tmpeigVecs((double*)eigVecs.get_data(), n, m);
-	getDims(aa, m, n);	
+	getDims(aa, m, n);
 	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
 	
-	return copy2bn( veToSliceAll(tmpeigVecs , tmpaa, trunc) );
+	auto tmp = fundDomain(tmpaa, pSlice, pFund);
+	return bp::make_tuple( copy2bn(tmp.first), copy2bn<ArrayXXi, int>(tmp.second));
     }
-
-    /* orbitAndFvWholeSlice  */
-	bp::tuple PYorbitAndFvWholeSlice(bn::ndarray a, bn::ndarray ve, double h,
-				     const int nstp, const string ppType,
-				     const int pos){
+    
+    bp::tuple PYredO2f(bn::ndarray aa, int pSlice, int pFund){
 	int m, n;
+	getDims(aa, m, n);
+	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
+	
+	auto tmp = redO2f(tmpaa, pSlice, pFund);
+	return bp::make_tuple( copy2bn(std::get<0>(tmp)),
+			       copy2bn<ArrayXXi, int>(std::get<1>(tmp)),
+			       copy2bn(std::get<2>(tmp))
+			       );
+    }
+    
+    bn::ndarray PYredV(bn::ndarray v, bn::ndarray a, int p, bool toY){
+	int m, n;
+	getDims(v, m, n);
+	Map<MatrixXd> tmpv((double*)v.get_data(), n, m);
 	getDims(a, m, n);
-	Map<ArrayXd> tmpa((double*)a.get_data(), n*m);
-	getDims(ve, m, n);
-	Map<ArrayXXd> tmpve((double*)ve.get_data(), n, m);
-	auto tmpav = orbitAndFvWholeSlice(tmpa, tmpve, h, nstp, ppType, pos);
+	Map<VectorXd> tmpa((double*)a.get_data(), n * m);
 
-	return bp::make_tuple(copy2bn(tmpav.first), copy2bn(tmpav.second));
-    }
-
-    /* reduceReflection */
-    bn::ndarray PYreduceReflection(bn::ndarray aa){
-	int m, n;
-	getDims(aa, m, n);
-	Map<ArrayXXd> tmpaa((double*)aa.get_data(), n, m);
-	return copy2bn(reduceReflection(tmpaa));
-    }
-
-    /* reflectVe */
-    bn::ndarray PYreflectVe(bn::ndarray ve, bn::ndarray x){
-	int m, n;
-	getDims(ve, m, n);
-	int m2, n2;
-	getDims(x, m2, n2);
-	
-	Map<MatrixXd> tmpve((double*)ve.get_data(), n, m);
-	Map<ArrayXd> tmpx((double*)x.get_data(), m2*n2);
-	return copy2bn( reflectVe(tmpve, tmpx) );
-    }
-
-    /* reflectVeAll */
-    bn::ndarray PYreflectVeAll(bn::ndarray ve, bn::ndarray x, int trunc){
-	int m, n;
-	getDims(ve, m, n);
-	int m2, n2;
-	getDims(x, m2, n2);
-	Map<MatrixXd> tmpve((double*)ve.get_data(), n, m);
-	Map<MatrixXd> tmpx((double*)x.get_data(), n2, m2);
-	return copy2bn( reflectVeAll(tmpve, tmpx, trunc) );
+	return copy2bn( redV(tmpv, tmpa, p, toY) );
     }
     
     bn::ndarray PYcalMag(bn::ndarray aa){
@@ -234,80 +185,6 @@ public:
 			      );
     }
 
-    
-    bp::tuple PYredSO2(bn::ndarray aa, int p, bool toY){
-	int m, n;
-	getDims(aa, m, n);
-	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
-	
-	auto tmp = redSO2(tmpaa, p, toY);
-	return bp::make_tuple(copy2bn(tmp.first), 
-			      copy2bn(tmp.second)
-			      );
-    }
-
-    bp::tuple PYfundDomain(bn::ndarray aa, int pSlice, int pFund){
-	int m, n;
-	getDims(aa, m, n);
-	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
-	
-	auto tmp = fundDomain(tmpaa, pSlice, pFund);
-	return bp::make_tuple( copy2bn(tmp.first),
-			       copy2bn<ArrayXXi, int>(tmp.second)
-			       );
-    }
-    
-    bp::tuple PYredO2f(bn::ndarray aa, int pSlice, int pFund){
-	int m, n;
-	getDims(aa, m, n);
-	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
-	
-	auto tmp = redO2f(tmpaa, pSlice, pFund);
-	return bp::make_tuple( copy2bn(std::get<0>(tmp)),
-			       copy2bn<ArrayXXi, int>(std::get<1>(tmp)),
-			       copy2bn(std::get<2>(tmp))
-			       );
-    }
-    
-    bn::ndarray PYredRef(bn::ndarray aa){
-	int m, n;
-	getDims(aa, m, n);
-	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
-
-	return copy2bn(redRef(tmpaa));
-    }
-
-    bp::tuple PYredO2(bn::ndarray aa, int p, bool toY){
-	int m, n;
-	getDims(aa, m, n);
-	Map<MatrixXd> tmpaa((double*)aa.get_data(), n, m);
-
-	auto tmp = redO2(tmpaa, p, toY);
-	return bp::make_tuple(copy2bn(tmp.first), 
-			      copy2bn(tmp.second)
-			      );
-    }
-
-    bn::ndarray PYredV(bn::ndarray v, bn::ndarray a, int p, bool toY){
-	int m, n;
-	getDims(v, m, n);
-	Map<MatrixXd> tmpv((double*)v.get_data(), n, m);
-	getDims(a, m, n);
-	Map<VectorXd> tmpa((double*)a.get_data(), n * m);
-
-	return copy2bn( redV(tmpv, tmpa, p, toY) );
-    }
-
-    bn::ndarray PYredV2(bn::ndarray v, bn::ndarray a){
-	int m, n;
-	getDims(v, m, n);
-	Map<MatrixXd> tmpv((double*)v.get_data(), n, m);
-	getDims(a, m, n);
-	Map<VectorXd> tmpa((double*)a.get_data(), n * m);
-
-	return copy2bn(redV2(tmpv, tmpa));
-    }
-
 };
 
 //////////////////////////////////////////////////////////////////////
@@ -318,51 +195,43 @@ public:
 BOOST_PYTHON_MODULE(py_ks) {
     bn::initialize();    
     bp::class_<KS>("KS", bp::init<int, double>()) ;
+
+    // the EIDr class
+    bp::class_<EIDr>("EIDr", bp::init<>())
+	.def_readonly("NCalCoe", &EIDr::NCalCoe)
+	.def_readonly("NReject", &EIDr::NReject)
+	.def_readonly("NCallF", &EIDr::NCallF)
+	.def_readonly("NSteps", &EIDr::NSteps)
+	.def_readonly("TotalTime", &EIDr::TotalTime)
+	.def_readonly("CoefficientTime", &EIDr::CoefficientTime)
+	.def_readwrite("rtol", &EIDr::rtol)
+	;
     
     bp::class_<pyKS, bp::bases<KS> >("pyKS", bp::init<int, double>())
 	.def_readonly("N", &pyKS::N)
 	.def_readonly("d", &pyKS::d)
-	.def_readwrite("rtol", &pyKS::rtol)
-	.def_readwrite("nu", &pyKS::nu)
-	.def_readwrite("mumax", &pyKS::mumax)
-	.def_readwrite("mumin", &pyKS::mumin)
-	.def_readwrite("mue", &pyKS::mue)
-	.def_readwrite("muc", &pyKS::muc)
-	.def_readwrite("NCalCoe", &pyKS::NCalCoe)
-	.def_readwrite("NReject", &pyKS::NReject)
-	.def_readwrite("NCallF", &pyKS::NCallF)
-	.def_readwrite("NSteps", &pyKS::NSteps)
-	.def_readwrite("Method", &pyKS::Method)
+	.def_readwrite("eidc", &pyKS::eidc)
+	.def_readwrite("eidc2", &pyKS::eidc2)
 	.def("Ts", &pyKS::PYTs)
 	.def("hs", &pyKS::PYhs)
 	.def("lte", &pyKS::PYlte)
-	.def("velocity", &pyKS::PYvelocity)
-	.def("velg", &pyKS::PYvelg)
-	.def("stab", &pyKS::PYstab)
-	.def("stabReq", &pyKS::PYstabReq)
+	.def("intgC", &pyKS::PYintgC)
+	.def("intgjC", &pyKS::PYintgjC)
 	.def("intg", &pyKS::PYintg)
 	.def("intgj", &pyKS::PYintgj)
-	.def("aintg", &pyKS::PYaintg)
-	.def("aintgj", &pyKS::PYaintgj)
-	.def("Reflection", &pyKS::PYreflection)
+	.def("velocity", &pyKS::PYvelocity)
+	.def("velReq", &pyKS::PYvelReq)
+	.def("stab", &pyKS::PYstab)
+	.def("stabReq", &pyKS::PYstabReq)
+	.def("reflect", &pyKS::PYreflect)
 	.def("half2whole", &pyKS::PYhalf2whole)
-	.def("Rotation", &pyKS::PYrotation)
-	.def("orbitToSlice", &pyKS::PYorbitToSlice)
-	.def("veToSlice", &pyKS::PYveToSlice)
-	.def("veToSliceAll", &pyKS::PYveToSliceAll)
-	.def("orbitAndFvWholeSlice", &pyKS::PYorbitAndFvWholeSlice)
-	.def("reduceReflection", &pyKS::PYreduceReflection)
-	.def("reflectVe", &pyKS::PYreflectVe)
-	.def("reflectVeAll", &pyKS::PYreflectVeAll)
-	.def("calMag", &pyKS::PYcalMag)
-	.def("toPole", &pyKS::PYtoPole)
+	.def("rotate", &pyKS::PYrotate)
 	.def("redSO2", &pyKS::PYredSO2)
 	.def("fundDomain", &pyKS::PYfundDomain)
 	.def("redO2f", &pyKS::PYredO2f)
-	.def("redRef", &pyKS::PYredRef)
-	.def("redO2", &pyKS::PYredO2)
 	.def("redV", &pyKS::PYredV)
-	.def("redV2", &pyKS::PYredV2)
+	.def("calMag", &pyKS::PYcalMag)
+	.def("toPole", &pyKS::PYtoPole)
 	;
 
 }
